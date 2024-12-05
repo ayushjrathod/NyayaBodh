d0009c0f5d60545ba74869a836b57f1fc1cd9dfb
ayushjrathod <clashofa1057@gmail.com>
2024-12-05 17:57:58 +0530

    minor changes in navbar

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,15 +1,14 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../Navbar/Navbar";
 import NewNavBar from "../Navbar/NewNavbar";
 
 const Layout = () => {
-    return (
-        <div>
-            {/* <Navbar /> */}
-            <NewNavBar />
-            <Outlet />
-        </div>
-    )
-}
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <NewNavBar />
+      <Outlet />
+    </div>
+  );
+};
 
 export default Layout;
