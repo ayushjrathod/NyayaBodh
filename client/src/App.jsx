9cd9647fc9681348f08d40c134493a49d414e6d8
ayushjrathod <clashofa1057@gmail.com>
2024-12-11 04:09:59 +0530

    s

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Card, CardBody, CardHeader, Spinner } from "@nextui-org/react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { RecoilRoot } from "recoil";
 import Layout from "./components/Layout/Layout";
 // import ChatPage from "./pages/Chatbot/chattest";
 import AgreementOfSaleForm from "./components/DocGen/AOS/AOS";
@@ -68,183 +67,181 @@ const App = () => {
   }
 
   return (
-    <RecoilRoot>
-      <Router>
-        <Routes>
-          {/* Protected Routes */}
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <LandingSearch />
-                <SiteChatbot />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/results"
-            element={
-              <ProtectedRoute>
-                <Layout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<Results />} />
-          </Route>
-          <Route
-            path="/chat/:id"
-            element={
-              <ProtectedRoute>
-                <Layout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<Chatbot />} />
-          </Route>
-          <Route
-            path="/recommend/:id"
-            element={
-              <ProtectedRoute>
-                <Layout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<Recommend />} />
-          </Route>
-          <Route
-            path="/resources"
-            element={
-              <ProtectedRoute>
-                <Layout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<Resources />} />
-          </Route>
-          <Route
-            path="/contact"
-            element={
-              <ProtectedRoute>
-                <Layout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<Contact />} />
-          </Route>
-          <Route
-            path="/result/:id"
-            element={
-              <ProtectedRoute>
-                <Layout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<SeprateResults />} />
-          </Route>
-          {/*legal doc gen routes */}
-          <Route
-            path="/docgen"
-            element={
-              <ProtectedRoute>
-                <Layout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<SelectionPage />} />
-          </Route>
-          <Route
-            path="/docgen/NDA"
-            element={
-              <ProtectedRoute>
-                <Layout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<NDAForm />} />
-          </Route>
-          <Route
-            path="/docgen/POA"
-            element={
-              <ProtectedRoute>
-                <Layout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<POA />} />
-          </Route>
-          <Route
-            path="/docgen/LLA"
-            element={
-              <ProtectedRoute>
-                <Layout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<LicenseAgreementForm />} />
-          </Route>
-          <Route
-            path="/docgen/ENDNCA"
-            element={
-              <ProtectedRoute>
-                <Layout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<EmployeeNDAForm />} />
-          </Route>
-          <Route
-            path="/docgen/DOSF"
-            element={
-              <ProtectedRoute>
-                <Layout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<DeedOfSaleOfFlat />} />
-          </Route>
-          <Route
-            path="/docgen/DOSL"
-            element={
-              <ProtectedRoute>
-                <Layout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<LandSaleDeedForm />} />
-          </Route>
-          <Route
-            path="/docgen/DOW"
-            element={
-              <ProtectedRoute>
-                <Layout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<WillDeedForm />} />
-          </Route>
-          <Route
-            path="/docgen/GenAIClause"
-            element={
-              <ProtectedRoute>
-                <Layout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<GenAIClause />} />
-          </Route>
-          <Route
-            path="/docgen/AOS"
-            element={
-              <ProtectedRoute>
-                <Layout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<AgreementOfSaleForm />} />
-          </Route>
-        </Routes>
-      </Router>
-    </RecoilRoot>
+    <Router>
+      <Routes>
+        {/* Protected Routes */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <LandingSearch />
+              <SiteChatbot />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/results"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Results />} />
+        </Route>
+        <Route
+          path="/chat/:id"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Chatbot />} />
+        </Route>
+        <Route
+          path="/recommend/:id"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Recommend />} />
+        </Route>
+        <Route
+          path="/resources"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Resources />} />
+        </Route>
+        <Route
+          path="/contact"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Contact />} />
+        </Route>
+        <Route
+          path="/result/:id"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<SeprateResults />} />
+        </Route>
+        {/*legal doc gen routes */}
+        <Route
+          path="/docgen"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<SelectionPage />} />
+        </Route>
+        <Route
+          path="/docgen/NDA"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<NDAForm />} />
+        </Route>
+        <Route
+          path="/docgen/POA"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<POA />} />
+        </Route>
+        <Route
+          path="/docgen/LLA"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<LicenseAgreementForm />} />
+        </Route>
+        <Route
+          path="/docgen/ENDNCA"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<EmployeeNDAForm />} />
+        </Route>
+        <Route
+          path="/docgen/DOSF"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<DeedOfSaleOfFlat />} />
+        </Route>
+        <Route
+          path="/docgen/DOSL"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<LandSaleDeedForm />} />
+        </Route>
+        <Route
+          path="/docgen/DOW"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<WillDeedForm />} />
+        </Route>
+        <Route
+          path="/docgen/GenAIClause"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<GenAIClause />} />
+        </Route>
+        <Route
+          path="/docgen/AOS"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<AgreementOfSaleForm />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 };
 
